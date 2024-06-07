c9166b4186b5f3aad3035010fecfc5612beb949a
David Liebovitz <DavidL@northwestern.edu>
2024-06-07 17:06:34 -0500

    working with password

--- basic.py
+++ basic.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 import asyncio
 import aiohttp
 import json
+from embedchain import App
 
 from prompts import system_prompt_expert_questions, expert1_system_prompt, expert2_system_prompt, expert3_system_prompt
 
@@ -131,58 +132,83 @@ def create_chat_completion(
     
     return completion
 
+def check_password() -> bool:
+    def password_entered() -> None:
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            app = App()
+            app.reset()
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    st.session_state.setdefault("password_correct", False)
+    if not st.session_state["password_correct"]:
+        st.text_input("Password", type="password", on_change=password_entered, key='password')
+        st.write("*Please contact David Liebovitz, MD if you need an updated password for access.*")
+        return False
+
+    if not st.session_state["password_correct"]:
+        st.text_input("Password", type="password", on_change=password_entered, key="password")
+        st.error("😕 Password incorrect")
+        return False
+
+    return True
+
 def main():
     st.title('Simultaneous LLM Queries')
     
-    # Obtain the initial query from the user
-    original_query = st.text_input('Original Query', 'How long should alendronate be held for tooth extractions?')
-    find_experts_messages = [{'role': 'system', 'content': system_prompt_expert_questions}, 
-                             {'role': 'user', 'content': original_query}]
-    if st.button('Begin Research'):
-        completion = create_chat_completion(messages=find_experts_messages, temperature=0.3, response_format="json_object")
-        with st.sidebar:
-            with st.expander("Experts Identified"):
-                st.write(f"**Response:**")
-                json_output = completion.choices[0].message.content
-                st.write(json_output)
-                experts, domains, expert_questions = extract_expert_info(json_output)
-                st.write(f"**Experts:** {experts}")
-                st.write(f"**Domains:** {domains}")
-                st.write(f"**Expert Questions:** {expert_questions}")
-        
-        updated_expert1_system_prompt = expert1_system_prompt.format(expert=experts[0], domain=domains[0])
-        updated_expert2_system_prompt = expert2_system_prompt.format(expert=experts[1], domain=domains[1])
-        updated_expert3_system_prompt = expert3_system_prompt.format(expert=experts[2], domain=domains[2])
-        updated_question1 = expert_questions[0]
-        updated_question2 = expert_questions[1]
-        updated_question3 = expert_questions[2]
-        
-        expert1_messages = [{'role': 'system', 'content': updated_expert1_system_prompt}, 
-                            {'role': 'user', 'content': updated_question1}]
-        expert2_messages = [{'role': 'system', 'content': updated_expert2_system_prompt}, 
-                            {'role': 'user', 'content': updated_question2}]
-        expert3_messages = [{'role': 'system', 'content': updated_expert3_system_prompt}, 
-                            {'role': 'user', 'content': updated_question3}]
-        
-        with st.spinner('Sending queries to experts...'):
-            expert_answers = asyncio.run(get_responses([expert1_messages, expert2_messages, expert3_messages]))
-        for i, response in enumerate(expert_answers):
-            with st.expander(f"{experts[i]} Perspective"):
-                st.write(response['choices'][0]['message']['content'])
-            # st.write(f"**Expert {i+1} Response:**")
-            # st.write(response['choices'][0]['message']['content'])
+    if check_password():
+    
+        # Obtain the initial query from the user
+        original_query = st.text_input('Original Query', 'How long should alendronate be held for tooth extractions?')
+        find_experts_messages = [{'role': 'system', 'content': system_prompt_expert_questions}, 
+                                {'role': 'user', 'content': original_query}]
+        if st.button('Begin Research'):
+            completion = create_chat_completion(messages=find_experts_messages, temperature=0.3, response_format="json_object")
+            with st.sidebar:
+                with st.expander("Experts Identified"):
+                    st.write(f"**Response:**")
+                    json_output = completion.choices[0].message.content
+                    st.write(json_output)
+                    experts, domains, expert_questions = extract_expert_info(json_output)
+                    st.write(f"**Experts:** {experts}")
+                    st.write(f"**Domains:** {domains}")
+                    st.write(f"**Expert Questions:** {expert_questions}")
+            
+            updated_expert1_system_prompt = expert1_system_prompt.format(expert=experts[0], domain=domains[0])
+            updated_expert2_system_prompt = expert2_system_prompt.format(expert=experts[1], domain=domains[1])
+            updated_expert3_system_prompt = expert3_system_prompt.format(expert=experts[2], domain=domains[2])
+            updated_question1 = expert_questions[0]
+            updated_question2 = expert_questions[1]
+            updated_question3 = expert_questions[2]
+            
+            expert1_messages = [{'role': 'system', 'content': updated_expert1_system_prompt}, 
+                                {'role': 'user', 'content': updated_question1}]
+            expert2_messages = [{'role': 'system', 'content': updated_expert2_system_prompt}, 
+                                {'role': 'user', 'content': updated_question2}]
+            expert3_messages = [{'role': 'system', 'content': updated_expert3_system_prompt}, 
+                                {'role': 'user', 'content': updated_question3}]
+            
+            with st.spinner('Waiting for experts to respond...'):
+                expert_answers = asyncio.run(get_responses([expert1_messages, expert2_messages, expert3_messages]))
+            for i, response in enumerate(expert_answers):
+                with st.expander(f"{experts[i]} Perspective"):
+                    st.write(response['choices'][0]['message']['content'])
+                # st.write(f"**Expert {i+1} Response:**")
+                # st.write(response['choices'][0]['message']['content'])
 
-    # query1 = st.text_input('Query 1', 'What is the capital of France?')
-    # query2 = st.text_input('Query 2', 'Explain the theory of relativity.')
-    # query3 = st.text_input('Query 3', 'What are the benefits of a ketogenic diet?')
+        # query1 = st.text_input('Query 1', 'What is the capital of France?')
+        # query2 = st.text_input('Query 2', 'Explain the theory of relativity.')
+        # query3 = st.text_input('Query 3', 'What are the benefits of a ketogenic diet?')
 
-    # if st.button('Send Queries'):
-    #     queries = [query1, query2, query3]
-    #     responses = asyncio.run(get_responses(queries))
+        # if st.button('Send Queries'):
+        #     queries = [query1, query2, query3]
+        #     responses = asyncio.run(get_responses(queries))
 
-    #     for i, response in enumerate(responses):
-    #         st.write(f"**Response {i+1}:**")
-    #         st.write(response['choices'][0]['message']['content'])
+        #     for i, response in enumerate(responses):
+        #         st.write(f"**Response {i+1}:**")
+        #         st.write(response['choices'][0]['message']['content'])
 
 if __name__ == '__main__':
     main()
