bf7dfc2300f4c7c1686c1227694153665a1f6a53
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2024-06-09 21:20:08 -0500

    updated about

--- basic.py
+++ basic.py
@@ -281,13 +281,15 @@ def check_password() -> bool:
 
 def main():
     st.title('Helpful Answers with AI!')
-    st.info("""This app retrieves content from specific internet domains for an initial answer and asks AI personas their 
-            opinions on the topic. Approaches shown to improve outputs like [chain of thought](https://arxiv.org/abs/2201.11903), 
-            [expert rephrasing](https://arxiv.org/html/2311.04205v2), and [chain of verification](https://arxiv.org/abs/2309.11495)
-            are applied to improve the quality of the responses and to reduce hallucination. Web sites are identified,processed and 
-            content selectively retrieved for answers using [Real-Time Web Search](https://rapidapi.com/letscrape-6bRBa3QguO5/api/real-time-web-search) 
-            and the [EmbedChain](https://embedchain.ai/) library. 
-            """)
+    with st.expander("Abou this app"):
+        st.info("""This app retrieves content from specific internet domains for an initial answer and asks AI personas their 
+                opinions on the topic. Approaches shown to improve outputs like [chain of thought](https://arxiv.org/abs/2201.11903), 
+                [expert rephrasing](https://arxiv.org/html/2311.04205v2), and [chain of verification](https://arxiv.org/abs/2309.11495)
+                are applied to improve the quality of the responses and to reduce hallucination. Web sites are identified,processed and 
+                content selectively retrieved for answers using [Real-Time Web Search](https://rapidapi.com/letscrape-6bRBa3QguO5/api/real-time-web-search) 
+                and the [EmbedChain](https://embedchain.ai/) library. The LLM model is [GPT-4o](https://openai.com/index/hello-gpt-4o/) from OpenAI.
+                App author is David Liebovitz, MD
+                """)
     app = App()
     if "snippets" not in st.session_state:
         st.session_state["snippets"] = []
